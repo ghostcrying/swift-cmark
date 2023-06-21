8bc240148cd7d4941262e68b13b8ffc0a093c860
陈卓 <zhuo.chen@metawall.ai>
2023-06-21 16:09:21 +0800

    feat: fix carthage bug

--- Sources/cmark_gfm.h
+++ Sources/cmark_gfm.h
@@ -13,11 +13,42 @@ FOUNDATION_EXPORT double MarkdownVersionNumber;
 //! Project version string for Markdown.
 FOUNDATION_EXPORT const unsigned char MarkdownVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <cmark_gfm/PublicHeader.h>
-//
-//#ifndef cmark_gfm_h
-//#define cmark_gfm_h
-//
-//#include "CAtomic.h"
-//
-//#endif /* cmark_gfm_h */
+#ifndef cmark_gfm_h
+#define cmark_gfm_h
+
+#include "buffer.h"
+#include "chunk.h"
+#include "cmark_ctype.h"
+#include "cmark-gfm_config.h"
+#include "cmark-gfm_version.h"
+#include "cmark-gfm-extension_api.h"
+#include "cmark-gfm.h"
+#include "export.h"
+#include "footnotes.h"
+#include "houdini.h"
+#include "html.h"
+#include "inlines.h"
+#include "iterator.h"
+#include "map.h"
+#include "mutex.h"
+#include "node.h"
+#include "parser.h"
+#include "plugin.h"
+#include "references.h"
+#include "registry.h"
+#include "render.h"
+#include "scanners.h"
+#include "syntax_extension.h"
+#include "utf8.h"
+
+#include "cmark-gfm-core-extensions.h"
+#include "autolink.h"
+#include "ext_scanners.h"
+#include "strikethrough.h"
+#include "table.h"
+#include "tagfilter.h"
+#include "tasklist.h"
+
+#include "CAtomic.h"
+
+#endif /* cmark_gfm_h */
